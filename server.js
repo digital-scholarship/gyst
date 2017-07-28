590a8970de1783482a36b5555bf8b525d1b27cf3
Peter Neish <peter.neish@gmail.com>
2017-07-28 17:49:59 +1000

    adding in more routes etc

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const base58 = require('./base58.js');
 const path = require('path');
-
+const csv = require('csvtojson');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -14,16 +14,22 @@ app.get('/', function (rq, res){
     res.send('hello workd')
 })
 
-app.get('/report/:id', function (rq, res){
+app.get('/result/:id', function (rq, res){
     // route to the report for an id 
     res.send('report' + rq.params.id)
 })
 
-app.post('/api/', function (rq, res){
-    res.send('hello workd')
+app.get('/questions', function(rq, res){
+    res.sendFile(__dirname + '/public/questions/dobject.json');
 })
 
 
-app.listen(3000, function (){
-    console.log('listening on port 3000')
+app.post('/result', function (rq, res){
+    res.send('hello workd')
+        console.log('received : ' + rq.params)
+})
+
+
+app.listen(8080, function (){
+    console.log('listening on port 8080')
 })
